4b6ef826d76081bd428fe35d432893c40d2953f9
gatear <razvangabrielgatea@gmail.com>
2018-07-25 17:50:51 +0300

    Started basic Spark ML operations

--- src/main/scala/template/spark/Main.scala
+++ src/main/scala/template/spark/Main.scala
@@ -1,27 +1,54 @@
 package template.spark
 
-import org.apache.spark.sql.functions._
+import org.apache.spark.ml.linalg.{DenseVector, Matrix, SparseVector}
+import org.apache.spark.ml.stat.Correlation
+import org.apache.spark.sql.{Row, RowFactory}
 
-final case class Person(firstName: String, lastName: String,
-                        country: String, age: Int)
+final case class Person(firstName: String, lastName: String, country: String, age: Int)
 
-object Main extends InitSpark {
+
+object Main extends SparkEnv {
+
+  //Overloading operators for easy row,vector manipulation
+  //https://spark.apache.org/docs/2.2.0/ml-statistics.html
+
+  def ~~>[T](that: Vector[T]): Row = {
+    RowFactory.create(that)
+  }
+
+  def \\>(that: Array[Double]): DenseVector = {
+    new DenseVector(that)
+  }
+
+  def ||>(that: Array[Double]): SparseVector = {
+    val nonZeroThat = that.zipWithIndex.filter(_._1 != 0.0)
+    new SparseVector(that.length, nonZeroThat.map(t => t._2), nonZeroThat.map(t => t._1))
+  }
+
+
   def main(args: Array[String]) = {
-    import spark.implicits._
 
+    import spark.implicits._
     val version = spark.version
     println("SPARK VERSION = " + version)
 
-    val sumHundred = spark.range(1, 101).reduce(_ + _)
-    println(f"Sum 1 to 100 = $sumHundred")
+    val data = Seq(
+      ||>(Array(0.0, 1.0, 1.0, 4.0, 4.0)),
+      \\>(Array(1.0, 9.0, 7.0, 0.0, 0.0)),
+      ||>(Array(0.0, 0.0, 8.0, 8.0, 8.0))
+    )
 
-    println("Reading from csv file: people-example.csv")
-    val persons = reader.csv("people-example.csv").as[Person]
-    persons.show(2)
-    val averageAge = persons.agg(avg("age"))
-                     .first.get(0).asInstanceOf[Double]
-    println(f"Average Age: $averageAge%.2f")
+    val df = data.map(Tuple1.apply).toDF("features")
 
-    close
+    val Row(coeff1: Matrix) = Correlation.corr(df, "features").head
+
+    //String interpolation
+    println(s"Pearson correlation matrix:\n $coeff1")
+
+    val Row(coeff2: Matrix) = Correlation.corr(df, "features", "spearman").head
+
+    //String interpolation
+    println(s"Spearman correlation matrix:\n $coeff2")
+
   }
 }
--- src/main/scala/template/spark/SparkEnv.scala
+++ src/main/scala/template/spark/SparkEnv.scala
@@ -3,23 +3,26 @@ package template.spark
 import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.sql.SparkSession
 
-trait InitSpark {
+trait SparkEnv {
   val spark: SparkSession = SparkSession.builder()
-                            .appName("Spark example")
+                            .appName("Spark app")
                             .master("local[*]")
                             .config("option", "some-value")
                             .getOrCreate()
 
   val sc = spark.sparkContext
   val sqlContext = spark.sqlContext
+
   def reader = spark.read
                .option("header",true)
                .option("inferSchema", true)
                .option("mode", "DROPMALFORMED")
+
   def readerWithoutHeader = spark.read
                             .option("header",true)
                             .option("inferSchema", true)
                             .option("mode", "DROPMALFORMED")
+
   private def init = {
     sc.setLogLevel("ERROR")
     Logger.getLogger("org").setLevel(Level.ERROR)
@@ -27,6 +30,7 @@ trait InitSpark {
     LogManager.getRootLogger.setLevel(Level.ERROR)
   }
   init
+
   def close = {
     spark.close()
   }
